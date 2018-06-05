6e84b2cc3a2e1e89cc0b017ba5cf6fc47f8d319c
dottjt <julius.reade@gmail.com>
2018-06-05 21:01:06 +1000

    added basic styling/layout for homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openInviteForm: false,
+    }
+  }
+
+  onOpenInviteForm() {
+    this.setState({})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="page">
+
+        <nav className="nav">
+          <h1 className="nav__title">BROCCOLI & CO.</h1>
+        </nav>
+
+        <main className="hero">
+          <h2 className="hero__title">A better way to enjoy every day.</h2>
+          <h3 className="hero__title__secondary">Be the first to know when we launch.</h3>
+
+          <button className="hero__button">Request an invite</button>
+        </main>
+
+        <footer className="footer">
+          <p>Made with &#x1F49C; in Melbourne.</p>
+          <p>&copy; 2018 Broccoli & Co. All rights reserved.</p>
+        </footer>
+
       </div>
     );
   }
